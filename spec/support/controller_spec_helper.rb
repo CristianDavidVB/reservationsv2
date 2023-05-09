b999883e486b9ad070aacee9440c47293aeddb65
Cristian Villanova <cristiandavidv@hotmail.com>
2023-05-08 21:27:07 -0500

    change workflow

--- spec/support/controller_spec_helper.rb
+++ spec/support/controller_spec_helper.rb
@@ -4,7 +4,7 @@ module ControllerSpecHelper
   end
 
   def expired_token_generator(user_id)
-    JsonWebToken.encode({ user_id: user.id } , (Time.now.to_i - 10))
+    JsonWebToken.encode({ user_id: user.id } , (Time.current.to_i - 10))
   end
 
   def valid_headers
